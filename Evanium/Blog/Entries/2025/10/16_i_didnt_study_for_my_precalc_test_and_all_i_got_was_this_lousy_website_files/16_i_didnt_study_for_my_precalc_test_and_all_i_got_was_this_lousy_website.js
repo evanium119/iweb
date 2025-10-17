bf65ef398ff3bdbaade9ab9f96651087b02badfa
evanium119 <evan_c_sullivan@disroot.org>
2025-10-16 21:06:29 -0400

    Add files via upload

---
+++ Evanium/Blog/Entries/2025/10/16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20251016
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,199),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_1.png'},{rect:new IWRect(2,-2,296,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_2.png'},{rect:new IWRect(298,-2,4,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_3.png'},{rect:new IWRect(298,2,4,199),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_4.png'},{rect:new IWRect(298,201,4,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_5.png'},{rect:new IWRect(2,201,296,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_6.png'},{rect:new IWRect(-2,201,4,4),url:'16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/stroke_7.png'}],new IWSize(300,203))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_website_files/16_i_didnt_study_for_my_precalc_test_and_all_i_got_was_this_lousy_websiteMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
